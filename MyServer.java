bfcdf305376df836d8c0f03251b7753a81801de7
manankshastri <manank.shastri@gmail.com>
2017-12-17 12:23:55 -0500

    l

--- MyServer.java
+++ MyServer.java
@@ -5,7 +5,7 @@ public class MyServer {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         try {//Keep this number. For the pace server this is the number we must use.
-            serverSocket = new ServerSocket(4040);
+            serverSocket = new ServerSocket(22);
             Socket clientSocket = null;
             clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
